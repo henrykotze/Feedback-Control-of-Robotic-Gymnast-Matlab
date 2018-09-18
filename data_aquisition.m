f557716b0a1b7f3f9128f31ecd37830712b5e7cf
Henry Kotze <19231865@sun.ac.za>
2018-09-18 15:17:22 +0200

    tbc: determining the damping ratio

--- data_aquisition.m
+++ data_aquisition.m
@@ -4,7 +4,7 @@
 % time,q1,q2,tau
 
 % file name:
-file_name = 'C:\Users\Henry\Desktop\Skripsie\software\test1.csv';
+file_name = 'C:\Users\Henry\Desktop\Skripsie\Feedback-Control-of-Robotic-Gymnast-MCU\test2.csv';
 
 % time increments
 time_steps = 1e-3;
@@ -12,7 +12,7 @@ system_info = csvread(file_name,2);
 
 time = system_info(1:end,1);
 q1 = (system_info(1:end,2)-1570).*0.00153;
-q2 = system_info(1:end,3).*0.01570796326;
+q2 = system_info(1:end,3).*0.01570796326/2;
 tau = system_info(1:end,4);
 
 % three point central difference
@@ -39,18 +39,33 @@ figure(1);
 plot(time,q1,'.');
 title('$\theta$ with respect to time','Interpreter','latex','FontSize',12)
 yticks([-2*pi -pi 0 pi 2*pi]);
-ylabel('$\theta$ -[rad]','Interpreter','latex','FontSize',12);
-xlabel('time [s]','Interpreter','latex','FontSize',12);
+ylabel('$\theta$ [rad]','Interpreter','latex','FontSize',12);
+xlabel('time [ms]','Interpreter','latex','FontSize',12);
 yticklabels({'-1.5\pi','-\pi','-0.5\pi','0','0.5\pi','\pi','1.5\pi'})
 yticks([-1.5*pi -1*pi -0.5*pi 0 0.5*pi 1*pi 1.5*pi]);
 grid on
 
+% Using Logarithmic decrement to determine the damping and natural
+% frequency
+[x,y] = ginput(7);
+
+%determine the average difference between points
+del_time = ( ( x(2:end,1)'-x(1:end-1,1))/ 6 )./1000;
+
+del = 1/6*log( x(1)'/x(6)' )
+
+damping_ratio_q1 = del/sqrt(4*pi^2+del^2)
+
 
 figure(2);
+
 plot(time,q2,'.');
 title('$\phi$ with respect to time ','Interpreter','latex','FontSize',12)
 ylabel('$\phi$ [rad]','Interpreter','latex','FontSize',12);
-xlabel('time [s]','Interpreter','latex','FontSize',12);
+xlabel('time [ms]','Interpreter','latex','FontSize',12);
 yticks([-1.5*pi -pi -0.5*pi 0 0.5*pi pi 1.5*pi]);
 yticklabels({'-1.5\pi', '-\pi','-0.5\pi','0','0.5\pi','\pi','1.5\pi'})
 grid on
+
+
+[x,y] = ginput(3);
