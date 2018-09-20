75b955c7d7bd9538832f277d4bd3dff3240d5d57
Henry Kotze <19231865@sun.ac.za>
2018-09-20 14:09:02 +0200

    moved to matlab 2017b & FFTs added

--- data_aquisition.m
+++ data_aquisition.m
@@ -3,17 +3,31 @@
 % Data structure within .csv file:
 % time,q1,q2,tau
 
-% file name:
-file_name = 'C:\Users\Henry\Desktop\Skripsie\Feedback-Control-of-Robotic-Gymnast-MCU\test2.csv';
+% file name for q1
+file_name_q1 = 'C:\Users\Henry\Desktop\Skripsie\Feedback-Control-of-Robotic-Gymnast-MCU\q1_response6.csv';
+
+% file name for q2
+file_name_q2 = 'C:\Users\Henry\Desktop\Skripsie\Feedback-Control-of-Robotic-Gymnast-MCU\q2_response4.csv';
 
 % time increments
 time_steps = 1e-3;
-system_info = csvread(file_name,2);
 
-time = system_info(1:end,1);
-q1 = (system_info(1:end,2)-1570).*0.00153;
-q2 = system_info(1:end,3).*0.01570796326/2;
-tau = system_info(1:end,4);
+% response information
+system_info_q1 = csvread(file_name_q1,2);
+system_info_q2 = csvread(file_name_q2,2);
+
+% time for q1
+time_q1 = system_info_q1(1:end,1);
+% time for q2
+time_q2 = system_info_q2(1:end,1);
+
+% q1 response
+q1 = (system_info_q1(1:end,2)-1570).*0.00153;
+
+% q2 response
+q2 = system_info_q2(1:end,3).*0.01570796326/2;
+
+tau = system_info_q1(1:end,4);
 
 % three point central difference
 q1dot = cent_diff_n(q1,time_steps,3);
@@ -23,8 +37,8 @@ q2dotdot = cent_diff_n(q2dot,time_steps,3);
 
 %% Data Cleaning
 % Removing points outside boundary:
-i = find(q1(:,1) > 2*pi);
-j = find(q1(:,1) < -2*pi);
+i = find(q1(:,1) > 0.6*pi);
+j = find(q1(:,1) < -0.6*pi);
 q1(i) = 0;
 q1(j) = 0;
 
@@ -34,38 +48,64 @@ j = find(q2(:,1) < -2*pi);
 q2(i) = 0;
 q2(j) = 0;
 
+% smoothing function to remove any noise
+q1 = smooth(q1);
+q2 = smooth(q2);
 
+%  Response for q1
 figure(1);
-plot(time,q1,'.');
-title('$\theta$ with respect to time','Interpreter','latex','FontSize',12)
+plot(time_q1,q1,'.r');
+title('System Response from Initial Conditions with $\phi = 0 $ ','Interpreter','latex','FontSize',12)
 yticks([-2*pi -pi 0 pi 2*pi]);
 ylabel('$\theta$ [rad]','Interpreter','latex','FontSize',12);
 xlabel('time [ms]','Interpreter','latex','FontSize',12);
 yticklabels({'-1.5\pi','-\pi','-0.5\pi','0','0.5\pi','\pi','1.5\pi'})
 yticks([-1.5*pi -1*pi -0.5*pi 0 0.5*pi 1*pi 1.5*pi]);
 grid on
+xt = get(gca, 'XTick');
+set(gca, 'FontSize', 12);
+yt = get(gca, 'YTick');
+set(gca, 'FontSize', 12);
 
-% Using Logarithmic decrement to determine the damping and natural
-% frequency
-[x,y] = ginput(7);
-
-%determine the average difference between points
-del_time = ( ( x(2:end,1)'-x(1:end-1,1))/ 6 )./1000;
-
-del = 1/6*log( x(1)'/x(6)' )
-
-damping_ratio_q1 = del/sqrt(4*pi^2+del^2)
-
-
-figure(2);
-
-plot(time,q2,'.');
-title('$\phi$ with respect to time ','Interpreter','latex','FontSize',12)
+% Response for q2
+figure(2)
+plot(time_q2,q2,'b.');
+title('System Response from Initial Conditions with $\theta = 0 $ ','Interpreter','latex','FontSize',12)
+yticks([-2*pi -pi 0 pi 2*pi]);
 ylabel('$\phi$ [rad]','Interpreter','latex','FontSize',12);
 xlabel('time [ms]','Interpreter','latex','FontSize',12);
-yticks([-1.5*pi -pi -0.5*pi 0 0.5*pi pi 1.5*pi]);
-yticklabels({'-1.5\pi', '-\pi','-0.5\pi','0','0.5\pi','\pi','1.5\pi'})
+yticklabels({'-1.5\pi','-\pi','-0.5\pi','0','0.5\pi','\pi','1.5\pi'})
+yticks([-1.5*pi -1*pi -0.5*pi 0 0.5*pi 1*pi 1.5*pi]);
 grid on
+xt = get(gca, 'XTick');
+set(gca, 'FontSize', 12);
+yt = get(gca, 'YTick');
+set(gca, 'FontSize', 12);
+
+% filter to use: moving,lowess, loess, sgolay,rlowess,rloess
+
+abs_fft_q1 = abs(fft(q1));
+abs_fft_q2 = abs(fft(q2));
 
 
-[x,y] = ginput(3);
+
+
+
+
+
+
+
+
+
+figure(3);
+plot(abs_fft_q1(1:100),'-*r', 'LineWidth',2)
+grid on
+hold on
+plot(abs_fft_q2(1:100),'-*b', 'LineWidth',2)
+title('Frequency Content of Initial Condition System Responses','Interpreter','latex','FontSize',12)
+ylabel('Magnitude ','Interpreter','latex','FontSize',12);
+xlabel('Frequency [Hz]','Interpreter','latex','FontSize',12);
+legend({'System Response with $\phi = 0$ rad','System Response with  $\theta = 0$'},'Interpreter','latex','FontSize',12)
+
+
+
